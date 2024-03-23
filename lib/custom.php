734a906767c8f1e547dd2ab984c19d634dbcb594
Thomas Skerbis <thomas.skerbis@mac.com>
2024-03-23 20:53:25 +0100

    Update custom.php

--- lib/custom.php
+++ lib/custom.php
@@ -1,31 +1,20 @@
 <?php
 namespace skerbis\terminal;
 
+use rex_path;
+
 class CustomCommands {
 
-    /***************************************************************
-     *                 Add Your Custom Command Here                *
-     ***************************************************************
-     *    note 1: Function Name is Command and return is Result    *
-     *    note 2: $a is array of arguments                         *
-     * *************************************************************/
 
-    public static function hi($a): string 
+    public static function basepath(): string 
     {
-        return 'Hi '.implode(' ', $a);
+        return rex_path::base();
     }
 
-    public static function md5($a): string
+    public static function console(): string 
     {
-        $input = implode(' ', $a);
-        if ($input)
-            return md5($input);
-        else
-            return 'write something, example:<br>md5 test';
+        return rex_path::base().'/bin/ php console';
     }
 
-    public static function developer(): string 
-    {
-        return 'SmartWF<br><a href="https://github.com/smartwf" target="_blank">github</a> &nbsp; &nbsp; <a href="mailto:hi@smartwf.ir" target="_blank">mail</a> &nbsp; &nbsp; <a href="http://twitter.com/smartwf" target="_blank">twitter</a>';
-    }
+
 }
